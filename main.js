ea8088bea2a02fd1229f6e0411ec27b2a54202a0
Adam Whitlock <moose62712@gmail.com>
2019-11-06 09:39:17 -0700

    used window.onload for codesandbox
    also added setInterval to make time update every second

--- main.js
+++ main.js
@@ -1,21 +1,33 @@
-console.log("SCRATCH LOADED")
+console.log("SCRATCH LOADED");
 
 function echoYear(dateObj, element) {
-    var getIntro = document.querySelector(element);
-    var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', era: 'long', hour: "numeric", minute: "numeric", second: "numeric", timeZoneName: "long" };
-    var setIntro = getIntro.innerHTML = `Welcome. Today's date is ${new Date().toLocaleDateString("en-US", options)}.`;
-};
-
-function getDateObj() {
-    var date = new Date();
-    var day = date.getDate();
-    var month = date.getMonth()+1;
-    var year = date.getFullYear();
-
-    return {month, day, year}
+  var getIntro = document.querySelector(element);
+  var options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    era: "long",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+    timeZoneName: "long"
+  };
+  var setIntro = (getIntro.innerHTML = `Welcome. Today's date is ${new Date().toLocaleDateString(
+    "en-US",
+    options
+  )}.`);
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-    echoYear(getDateObj(), "#hello")
- });
+function getDateObj() {
+  var date = new Date();
+  var day = date.getDate();
+  var month = date.getMonth() + 1;
+  var year = date.getFullYear();
 
+  return { month, day, year };
+}
+
+window.onload = setInterval(function() {
+  echoYear(getDateObj(), "#hello");
+}, 1000);
