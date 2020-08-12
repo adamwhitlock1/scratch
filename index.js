20381062a1d35e9a75a4b6cdc227167dea0facb2
Adam Whitlock <awhitlock@dealerinspire.com>
2020-08-12 08:46:43 -0600

    clean up files

---
+++ index.js
@@ -0,0 +1,30 @@
+import chroma from "chroma-js";
+const randColor = chroma.random().hex();
+console.log(randColor);
+
+const colors = ["#ACADBC", "#9B9ECE", "#6665DD", "#473BF0", "#000500"];
+
+const textColors = colors.map((color) => {
+  if (chroma.contrast(color, "#FFFFFF") > 4.5) return "#FFFFFF";
+  if (chroma.contrast(color, "#000000") > 4.5) return "#000000";
+});
+
+console.log(textColors);
+
+const colorBlocks = Array.from(document.querySelectorAll(".color-block"));
+const colorTexts = Array.from(document.querySelectorAll(".color-text"));
+
+function getContrast(color) {
+  if (chroma.contrast(color, "#FFFFFF") > 4.5) return "#FFFFFF";
+  if (chroma.contrast(color, "#000000") > 4.5) return "#000000";
+}
+
+function genColor() {
+  colorBlocks.forEach((item, i) => {
+    const rand = chroma.random().hex();
+    item.style.backgroundColor = rand;
+    colorTexts[i].style.color = getContrast(rand);
+  });
+}
+
+genColor();
